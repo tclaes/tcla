5998213f1b9e1d8ee14a2cc64b48ad7d599db62e
z411419 <tom.claes82@gmail.com>
2017-03-17 11:24:11 +0100

    tcla aangepast

--- index.php
+++ index.php
@@ -18,11 +18,12 @@
         <div class="well">
           <span class="glyphicon glyphicon-phone"></span>
           <h3>Development</h3>
-          <p>Voorlopig wil ik de focus leggen op kleinere projecten zoals:
+          <p>Nood aan een nieuwe professionele website?
           <ul class="list-unstyled">
           <li>Single page website</li>
           <li>Small html5 apps</li>
           <li>Static website (max 5 pagina's)</li>
+          <li>Website met CMS<li>
           </p>
         </div>
       </div>
@@ -32,10 +33,10 @@
           <span class="glyphicon glyphicon-search"></span>
           <h3>Online marketing</h3>
           <h4>SEO</h4>
-          <p>Een aantal ingrepen op een website kunnen al wonderen verrichten om je website zichtbaar te krijgen binnen een zoekmachine. </p>
+          <p>Een aantal ingrepen op een website kunnen wonderen verrichten om je website zichtbaar te krijgen binnen een zoekmachine. </p>
           <h4>SEA</h4>
           <p>Als Google Partner kan ik je helpen met het optimaliseren van je SEA campagnes.</p>
-          <p>Een zo groot mogelijk rendement behalen binnen een bepaald budget.</p>
+          <p>Een zo groot mogelijk rendement behalen binnen een vooraf vastgesteld budget.</p>
         </div>
       </div>
 
@@ -43,43 +44,11 @@
         <div class="well">
           <span class="glyphicon glyphicon-film"></span>
           <h3>Multimedia</h3>
-          <p>Een topdesigner ben ik zeker niet, daar ga je andere personen voor moeten inschakelen.</p>
-          <p>Wat kan ik dan wel? <br> Met een goede basis Photoshop, Indesign kom je al een heel eind.</p>
+          <p>Ontwerp van print en web. </p>
           <p>Bekijk mijn realisaties hier: ...</p>
         </div>
       </div> 
 
-    </div>
-  </div>
-
-  <div class="section-a">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>Kleine projecten troeffire</h1>
-        <p>Alleen is het bijna onmogelijk grote projecten tot een goed einde te brengen. Daarom wil ik met voorlopig richten tot kleine zelfstandigen die nood hebben aan een duidelijke webpagina</p>
-        <p>Geen ingewikkelde cms maar een simpele site die de nodige informatie verschaft aan de bezoeker.</p>
-      </div>
-    </div>
-  </div>
-
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <img class="img-full" src="img/mobile.jpg">        
-      </div>
-      <div class="col-md-6">
-        <h1>Wat kan ik aanbieden:</h1>
-        <p>Voorlopig wil ik me bezig houden met kleinere projecten. Simpele maar degelijke en duidelijke websites / webapps waarmee de KMO geholpen wordt.</p>
-        <p>Ik ben geen topdesigner maar kan behoorlijk overweg met grafische toepassingen. Voor en simpel strak design ben je aan het juiste adres.</p>
-        <p>Alles is bespreekbaar, If I'm not up for the job I will tell you...</p>
-        <ul class="list-group">
-          <li class="list-group-item"><span class="glyphicon glyphicon-user"></span> Users</li>
-          <li class="list-group-item"><span class="glyphicon glyphicon-pencil"></span> Graphics</li>
-          <li class="list-group-item"><span class="glyphicon glyphicon-time"></span> Time</li>
-          <li class="list-group-item"><span class="glyphicon glyphicon-home"></span> Home</li>
-          <li class="list-group-item"><span class="glyphicon glyphicon-plus"></span> Extra</li>
-        </ul>
-      </div>
     </div>
   </div>
 
